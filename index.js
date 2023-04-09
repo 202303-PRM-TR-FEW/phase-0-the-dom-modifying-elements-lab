8be6a479c207561f1c58586b3b3464c29a791698
wasim altayyan <wasimtayyan@gmail.com>
2023-04-09 08:19:36 +0300

    assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const main = document.querySelector('#main');
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'H1'
+newHeader.setAttribute('id','victory')
+const name = ['wasim']
+newHeader.innerHTML = `${name[0]} is the champion`
+const body = document.querySelector('body');
+body.appendChild(newHeader);
